e9417fc7444a4d39063400372fa1455752ad4755
Meera <meerasunu266@gmail.com>
2022-05-06 12:08:39 +0530

    simple motor running and l293d connections

---
+++ may06_check_motor_running/may06_check_motor_running.ino
@@ -0,0 +1,31 @@
+// Motor A connections
+//int enA = 12;
+int in1 = 8;
+int in2 = 7;
+// Motor B connections
+//int enB = 3;
+int in3 = 5;
+int in4 = 4;
+
+void setup() {
+  // Set all the motor control pins to outputs
+//  pinMode(enA, OUTPUT);
+//  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  
+  // Turn off motors - Initial state
+//  digitalWrite(in1, LOW);
+//  digitalWrite(in2, LOW);
+//  digitalWrite(in3, LOW);
+//  digitalWrite(in4, LOW);
+}
+
+void loop() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
